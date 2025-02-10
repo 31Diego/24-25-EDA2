5bd35310f880844d3ac0a28c057e6706f2a600f2
31Diego <diegomartinezt31@gmail.com>
2025-02-10 11:55:31 +0100

    ejercicios

---
+++ entregas/finalEDA1/src/GestorPantalla.java
@@ -0,0 +1,16 @@
+
+class GestorPantalla {
+    private PantallaEntrelazada pantallaEntrelazada;
+
+    public GestorPantalla(int ancho, int alto) {
+        pantallaEntrelazada = new PantallaEntrelazada(ancho, alto);
+    }
+
+    public void establecerPixel(Coordenada coordenada, int color) {
+        pantallaEntrelazada.establecerPixel(coordenada, color);
+    }
+
+    public void renderizar() {
+        pantallaEntrelazada.renderizar();
+    }
+}
