5440dee27e6b7591bad1ace62e3c551494770ee6
Diego Martinez <114482789+31Diego@users.noreply.github.com>
2025-05-07 16:55:34 +0200

    reto006

---
+++ entregas/martinezDiego/v001/Indice.java
@@ -0,0 +1,98 @@
+package v001;
+
+public class Indice {
+    private String[] valores;
+    private int[][] posiciones;
+    private int[] contadores;
+    private int cantidadValores;
+
+    public Indice(int capacidadMaxima) {
+        valores = new String[capacidadMaxima];
+        posiciones = new int[capacidadMaxima][capacidadMaxima];
+        contadores = new int[capacidadMaxima];
+        cantidadValores = 0;
+    }
+
+    public void agregar(String valor, int posicion) {
+        int indiceValor = -1;
+        int i = 0;
+
+        while (i < cantidadValores && indiceValor == -1) {
+            if (valores[i].equals(valor)) {
+                indiceValor = i;
+            }
+            i++;
+        }
+
+        if (indiceValor == -1) {
+            valores[cantidadValores] = valor;
+            indiceValor = cantidadValores;
+            cantidadValores++;
+        }
+
+        posiciones[indiceValor][contadores[indiceValor]] = posicion;
+        contadores[indiceValor]++;
+    }
+
+    public int[] buscar(String valor) {
+        int indiceValor = -1;
+        int posicion = 0;
+
+        while (posicion < cantidadValores && indiceValor == -1) {
+            if (valores[posicion].equals(valor)) {
+                indiceValor = posicion;
+            }
+            posicion++;
+        }
+
+        if (indiceValor == -1) {
+            return new int[0];
+        }
+
+        int[] resultado = new int[contadores[indiceValor]];
+        for (int i = 0; i < contadores[indiceValor]; i++) {
+            resultado[i] = posiciones[indiceValor][i];
+        }
+
+        return resultado;
+    }
+
+    public boolean contiene(String valor) {
+        for (int i = 0; i < cantidadValores; i++) {
+            if (valores[i].equals(valor)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String[] obtenerTodos() {
+        String[] resultado = new String[cantidadValores];
+        for (int i = 0; i < cantidadValores; i++) {
+            resultado[i] = valores[i];
+        }
+        return resultado;
+    }
+
+    public void ordenar() {
+        for (int i = 0; i < cantidadValores - 1; i++) {
+            for (int j = 0; j < cantidadValores - 1 - i; j++) {
+                if (valores[j].compareTo(valores[j + 1]) > 0) {
+
+                    String tempValor = valores[j];
+                    valores[j] = valores[j + 1];
+                    valores[j + 1] = tempValor;
+
+                    int[] tempPosiciones = posiciones[j];
+                    posiciones[j] = posiciones[j + 1];
+                    posiciones[j + 1] = tempPosiciones;
+
+                    int tempContador = contadores[j];
+                    contadores[j] = contadores[j + 1];
+                    contadores[j + 1] = tempContador;
+                }
+            }
+        }
+    }
+
+}
